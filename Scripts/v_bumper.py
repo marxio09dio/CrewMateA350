265b6790bb415f7ca79eaacfed08c223ccafdd88
Márcio Araújo <38763806+marxio09dio@users.noreply.github.com>
2026-03-02 18:22:15 +0000

    bump

--- Scripts/v_bumper.py
+++ Scripts/v_bumper.py
@@ -22,40 +22,40 @@ def bump_version(version, increment_type):
 
 def update_json_files(new_version):
     # Bump version in package.json
-    with open('package.json', 'r') as f:
+    with open('../package.json', 'r') as f:
         package_json = json.load(f)
         package_json['version'] = new_version
-    with open('package.json', 'w') as f:
+    with open('../package.json', 'w') as f:
         json.dump(package_json, f, indent=2)
     print('Version bumped in package.json')
 
 
 def update_cargo_toml(new_version):
     # Read current content from Cargo.toml
-    with open('src-tauri/Cargo.toml', 'r') as f:
+    with open('../src-tauri/Cargo.toml', 'r') as f:
         cargo_content = f.read()
 
     # Bump version in Cargo.toml
     updated_cargo_content = re.sub(r'(\[package\][\s\S]*?version\s*=\s*")([\d.]+)(")', rf'\g<1>{new_version}\g<3>', cargo_content)
 
     # Write the modified content back to Cargo.toml
-    with open('src-tauri/Cargo.toml', 'w') as f:
+    with open('../src-tauri/Cargo.toml', 'w') as f:
         f.write(updated_cargo_content)
 
     print('Version bumped in src-tauri/Cargo.toml')
 
 def update_tauri_conf(new_version):
     # Bump version in tauri.conf.json
-    with open('src-tauri/tauri.conf.json', 'r') as f:
+    with open('../src-tauri/tauri.conf.json', 'r') as f:
         tauri_conf = json.load(f)
         tauri_conf['version'] = new_version
-    with open('src-tauri/tauri.conf.json', 'w') as f:
+    with open('../src-tauri/tauri.conf.json', 'w') as f:
         json.dump(tauri_conf, f, indent=2)
     print('Version bumped in src-tauri/tauri.conf.json')
 
 def main():
     # Read current version from package.json
-    with open('package.json', 'r') as f:
+    with open('../package.json', 'r') as f:
         package_json = json.load(f)
         current_version = package_json['version']
         print(f'Current versionings: {current_version}')
